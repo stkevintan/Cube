1e4d921329f89379435d9306e4d6abf3b467c5ea
stkevintan <stkevintan@foxmail.com>
2015-08-27 16:26:26 +0800

    format source code

--- src/libs/UserMusic.js
+++ src/libs/UserMusic.js
@@ -2,23 +2,24 @@ var Model = require('./Model');
 var Storage = require('./Storage');
 var st = new Storage();
 var um = {};
-function pack(entryList){
-  if(!entryList)return null;
-  return entryList.map(function(entry){
-    var ret = new Model.entry(entry);
-    for(var i = 0; i < ret.songList.length; i++)
-      ret.songList[i] = new Model.song(ret.songList[i]);
-    return ret;
-  });
+
+function pack(entryList) {
+    if (!entryList) return null;
+    return entryList.map(function(entry) {
+        var ret = new Model.entry(entry);
+        for (var i = 0; i < ret.songList.length; i++)
+            ret.songList[i] = new Model.song(ret.songList[i]);
+        return ret;
+    });
 }
 
-um.get = function(cb){
-  process.nextTick(function(){
-    var entryList = pack(st.get('scheme')) || [new Model.entry({
-      name:'正在播放',
-      creator:process.env.USER
-    })];
-    cb(null,entryList);
-  });
+um.get = function(cb) {
+    process.nextTick(function() {
+        var entryList = pack(st.get('scheme')) || [new Model.entry({
+            name: '正在播放',
+            creator: process.env.USER
+        })];
+        cb(null, entryList);
+    });
 }
-module.exports= um;
+module.exports = um;
--- src/main.js
+++ src/main.js
@@ -12,71 +12,69 @@ require('crash-reporter').start();
 
 var win = null;
 var loadSrc = function(sender) {
-  var loading = false;
-  var loadQue = new __.queue();
-  function load(opts, cb) {
-    var loadKeys = [];
-    if (!opts) loadKeys = Object.keys(MP);
-    else loadKeys = Object.keys(opts).filter(function(key) {
-        return (key in MP) && opts[key];
-      });
-    //load sources async
-    async.each(loadKeys, function(key, callback) {
-      //need bind ?
-      MP[key].loader(function(err,entryList){
-        if(err){
-          err.name=MP[key].name;
-          callback(err);
-          sender.send('source-load-error', err);
-        }
-        else {
-          MP[key].entryList=entryList;
-          callback(null);
-          console.log('source',__.inspect(MP[key],{depth:null}));
-          sender.send('source-loaded', MP[key]);
-        }
-      });
-    }, function(err){
-      console.warn('Failed to Load Source',err.stack);
-    });
-  }
+    var loading = false;
+    var loadQue = new __.queue();
 
-  return function(opts) {
-    loadQue.push(opts);
-    if (loading) return;
-    loading = true;
-    async.whilst(function() {
-      return !loadQue.empty();
-    }, function(cb) {
-      load(loadQue.pop(), cb);
-    }, function(err) {
-      loading = false;
-    });
-  };
+    function load(opts, cb) {
+        var loadKeys = [];
+        if (!opts) loadKeys = Object.keys(MP);
+        else loadKeys = Object.keys(opts).filter(function(key) {
+            return (key in MP) && opts[key];
+        });
+        //load sources async
+        async.each(loadKeys, function(key, callback) {
+            //need bind ?
+            MP[key].loader(function(err, entryList) {
+                if (err) {
+                    err.name = MP[key].name;
+                    callback(err);
+                    sender.send('source-load-error', err);
+                } else {
+                    MP[key].entryList = entryList;
+                    callback(null);
+                    console.log('source', __.inspect(MP[key], {depth: null}));
+                    sender.send('source-loaded', MP[key]);
+                }
+            });
+        }, function(err) {
+            console.warn('Failed to Load Source', err.stack);
+        });
+    }
+
+    return function(opts) {
+        loadQue.push(opts);
+        if (loading) return;
+        loading = true;
+        async.whilst(
+            function() {return !loadQue.empty();},
+            function(cb) {load(loadQue.pop(), cb);},
+            function(err) {loading = false;}
+        );
+    };
 };
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('ready', function() {
-  // Create the browser window.
-  win = new BrowserWindow({width: 1200, height: 600});
-  ipc.on('load-source',function(event,args){
-    (loadSrc(event.sender))(args);
-  });
-  // and load the index.html of the app.
-  win.loadUrl('file://' + __dirname + '/index.html');
-  win.setMenu(null);
-  win.openDevTools();
-  // Emitted when the window is closed.
-  win.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
+    // Create the browser window.
+    win = new BrowserWindow({width: 1200,height: 600});
+    ipc.on('load-source', function(event, args) {
+        (loadSrc(event.sender))(args);
+    });
+    // and load the index.html of the app.
+    win.loadUrl('file://' + __dirname + '/index.html');
+    win.setMenu(null);
+    win.openDevTools();
+    // Emitted when the window is closed.
+    win.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
 });
